19016081ad4683ed59c8aed5d382fe7030a72e4a
stel <glaceongirl2744@gmail.com>
2023-10-02 21:11:35 +0800

    initial

---
+++ load.js
@@ -0,0 +1,7 @@
+$(function(){
+      $("#left").load("../assets/left.html");
+      $("#right").load("../assets/right.html");
+      $("#updates").load("../assets/updates.html");
+      $("#head").load("../assets/header.html");
+      $("#foot").load("../assets/footer.html");
+});
