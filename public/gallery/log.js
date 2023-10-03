4acc7c805d5a4c151b3ccfeef877959d324892e5
stel <glaceongirl2744@gmail.com>
2023-10-03 23:09:43 +0800

    added gallery + some alt text

---
+++ public/gallery/log.js
@@ -0,0 +1,25 @@
+log = [
+    {
+        "img": "https://f2.toyhou.se/file/f2-toyhou-se/images/66432561_KTaIwJ1mvFG7bia.png", "date": "jun 16, 2023", "caption": "atlas",
+        "alt": "a digital halfbody drawing of atlas, a cat furry. her pose and expression are neutral, with her head tilted slightly downwards. her fur is a light tan color, and the fur on her head resembles hair, with sideswept bangs. she has two pigtails tied low behind her head, tied by two dark blue hair ties. she has a crescent moon shaped ahoge with a dark blue streak at the bottom. she is wearing a short, black cloak / capette, with a somewhat elaborate design on it with gold. underneath that, she has a white shirt on. she has one dark blue, star-shaped earring visible. the eyes are extremely stylized, being only 2 solid colors, and she has no visible mouth. the background is white with a dark blue rectangle. drawn on the rectangle is white cloud shapes, a crescent moon, and lots of sparkles."},
+    {
+        "img": "https://f2.toyhou.se/file/f2-toyhou-se/images/66888531_hiqVdYGvEJslOGU.png", "date": "jun 26, 2023", "caption": "leila",
+        "alt": "a digital drawing of a tv head character. the character is in a floaty pose, with their legs bent, and their arms hanging behind them. they have no visible body, so the form and pose is conveyed through the position of their clothing. the head is purpley-pink, with dials on the right side of the front. there are wings on either side of the tv, and 2 short antennae at the front. the screen displays static, and two closed, almost sad-looking cartoony white eyes over that. they are wearing a dark, short-sleeved collared shirt with a heart-shaped cutout in the chest, and a light pink overall dress with 2 white stripes at the very bottom, and white frills at the hem. they are wearing white, medium-length gloves. they have long-ish socks that are the same dark color as their shirt, with one pink bow behind each. they are wearing light pink, mary-jane-like shoes, with a bow on each. a long cord tail extends out from inside their dress, where a tail would start if they had a visible body. the tail curves upwards and is positioned between their front and back legs. the background is transparent."
+    }
+
+
+    //TEMPLATE
+    //{ "img": "https://via.placeholder.com/768", "date": "mm/dd/yyyy", "caption": "words", "tags": "digital_art"},
+
+    // { "img": "https://via.placeholder.com/768", "date": "11/30/2022", "caption": "test", "tags": "digital_art"}
+];
+
+//Entry codes:
+// da: digital art
+// ta: traditional art
+
+//MAKE SURE TO PUT , at the end or the table WILL break 
+//don't put , at the end if there isn't mroe
+
+// if you keep the "data" as blank e.g. "" then it's just going to be blank
+// you can put as much variable as possible think of it like adding new columns with new datas
