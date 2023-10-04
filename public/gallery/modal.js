103f2645387edf595731bb10b91e44d5e783b67a
stel <glaceongirl2744@gmail.com>
2023-10-04 14:52:20 +0800

    gallery tweaks

--- public/gallery/modal.js
+++ public/gallery/modal.js
@@ -1,10 +1,11 @@
-function toggleModal(IMAGE, caption) {
+function toggleModal(IMAGE) {
     var modal = document.getElementById('modalID'); // for convinience 
     // console.log(IMAGE); //prints image file directory; comment out if uneeded
     var modalm1 = document.createElement("img"); // creates <img>
     modalm1.style = 'width:80%; max-width: 100%; height: 80%; display: block; align-items: center; margin: auto; object-fit:contain;';
     modalm1.id = 'modalm1'; // added id to <img> so I can delete it later
     modalm1.src = IMAGE; //image source so in this case yourmom 
+
     modal.appendChild(modalm1);
     modal.style = 'display: block'; //show modal
 };
