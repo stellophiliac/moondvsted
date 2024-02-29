15c7de1359c61018074cb2a73feb3c59eda55627
stel <119149447+stellophiliac@users.noreply.github.com>
2024-02-29 19:26:23 +0800

    delete everything

--- public/load.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-      $("#left").load("/assets/left.html");
-      $("#right").load("/assets/right.html");
-      $("#updates").load("/assets/updates.html");
-      $("#head").load("/assets/header.html");
-      $("#foot").load("/assets/footer.html");
-});
