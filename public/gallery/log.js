64911981460fd5a85e2589366f5c09b1013ca212
stel <119149447+stellophiliac@users.noreply.github.com>
2024-03-07 18:56:16 +0800

    reading, links, gallery, colophon, now

--- public/gallery/log.js
+++ public/gallery/log.js
@@ -7,7 +7,7 @@ log = [
         "alt": "a digital drawing of a tv head character in a floaty position, almost like they're sitting back. the tv is purple with antennae and white wings on the sides. they wear a short-sleeved black shirt with a collar, and a light pink overall dress with lace at the bottom. they wear black knee-highs with pink mary jane shoes, with a bow on each. they wear long white gloves, and their arms hang behind them. a long cord tail with a plug at the end curves out and up from their dress. their body is invisible, so they're depicted as just floating clothes."
     },
     {
-        "img": "https://f2.toyhou.se/file/f2-toyhou-se/images/67458121_VA53rG7yBT33irW.png", "date": "jul 6, 2023", "caption": "<a href='https://www.youtube.com/watch?v=RJWiXvgVzKM'>pure light, pure dread, pure sudafed!</a>",
+        "img": "https://f2.toyhou.se/file/f2-toyhou-se/images/67458121_VA53rG7yBT33irW.png", "date": "jul -6, 2023", "caption": "<a href='https://www.youtube.com/watch?v=RJWiXvgVzKM'>pure light, pure dread, pure sudafed!</a>",
         "alt": "a digital drawing. the background is a photo of a bed, with a large window behind it showing a cityscape. a cat furry character (named sudafed) is drawn laying on the bed, looking towards the camera. their fur is white, with black hair, black eyes, and glasses. their hair covers one eye. they're wearing a purple hoodie and black shorts."
     },
     {
@@ -21,6 +21,14 @@ log = [
     {
         "img": "https://f2.toyhou.se/file/f2-toyhou-se/images/74054705_2FNmF7JjdiY8X0a.png", "date": "nov 27, 2023", "caption": " ",
         "alt": "the background is a photograph of a pool. the photo is noticably tinted bright purple. sudafed, a cat furry with white fur and black hair, sits on the edge of a pool, their feet in the water. they look at their phone. they wear a short-sleeve purple hoodie over a gray sweater. they wear fishnets and black-and-purple striped knee-highs."
+    },
+    {
+        "img":"https://f2.toyhou.se/file/f2-toyhou-se/images/75207488_f4JDK5OYBZcBlof.png?1703407312", "date": "dec 24, 2023", "caption":" ",
+        "alt":"a digital halfbody drawing of rusty, a furry that looks like a wolf or fox. he has desaturated red and white fur, with darker red hair. his eyes are heterochromic, with one yellow and one red. he wears an heart-patterned argyle sweater over a white collared shirt. he has a hand raised slightly and a happy expression on his face. a speech bubble with a heart is drawn next to him."
+    },
+    {
+        "img":"https://f2.toyhou.se/file/f2-toyhou-se/images/75211010_qespzOzCmwEIyir.png?1703421307", "date": "dec 24, 2023", "caption":" ",
+        "alt": "a digital halfbody drawing of holly and quinn. holly has light and dark blue fur, red eyes, and is wearing a dark blue sweater. they look past the camera and wave with one hand. quinn stands behind them, leaning forward slightly. she has white fur and pink hair, and wears a pink scarf and christmas hat. her hand is raised with one finger extended and she has a curious expression on her face."
     }
 
 
