5f8209f0f9f5fafc1d07474d9c565629eccc4683
stel <119149447+stellophiliac@users.noreply.github.com>
2024-03-31 16:06:21 +0800

    new member wooo

--- public/fan/oneshot/members.js
+++ public/fan/oneshot/members.js
@@ -1,10 +1,16 @@
 members = [
     {
-        name: "stel",
+        name: "Stel",
         country: "China",
         email: "stel@disroot.org",
         site: "https://moondvsted.neocities.org",
         favchar: "Lamplighter & George"
+    },
+    {
+        name: "Marisa",
+        country: "United States",
+        email: "chelan@gooberweb.xyz",
+        favchar: "Silver"
     }
     
 ]
