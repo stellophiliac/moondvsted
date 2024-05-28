d925db14e1b13789e15db4b58c93c51ce6846673
stel <119149447+stellophiliac@users.noreply.github.com>
2024-05-28 22:35:44 +0800

    lets see if this works

---
+++ src/fan/oneshot/members.js
@@ -0,0 +1,39 @@
+members = [
+    {
+        name: "Stel",
+        country: "China",
+        email: "stel@disroot.org",
+        site: "https://moondvsted.neocities.org",
+        favchar: "Lamplighter & George"
+    },
+    {
+        name: "Marisa",
+        country: "United States",
+        email: "chelan@gooberweb.xyz",
+        favchar: "Silver"
+    },
+    {
+        name: "Ciel",
+        country: "United States",
+        favchar: "Lamplighter, Silver, Niko"
+    },
+    { 
+        name: "Lindsay",
+        country: "United States",
+        favchar: "Everyone"
+    },
+    {
+        name:"Maggie",
+        country: "United States",
+        favchar:"Niko",
+        site:"https://magneticdogz.neocities.org"
+    },
+    {
+        name:"Flonne",
+        country: "United States",
+        email: "flonnepocket@gmail.com",
+        site: "https://vivarism.net/",
+        favchar: "Niko, The Player"
+    }
+    
+]
