b6e5c7da6766c469e0882c445a6798b630a2714a
stel <stel@disroot.org>
2024-08-20 17:53:22 +0800

    new blog post + atom feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 var moment = require('moment'); // require
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPlugin(EleventyRenderPlugin);
+    eleventyConfig.addPlugin(pluginRss);
 
     //passthrough
 
